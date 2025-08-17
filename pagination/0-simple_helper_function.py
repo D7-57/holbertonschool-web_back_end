0c6b0528a7d5067d032c3098674daa74a5e2607f
D7-57 <10665@holbertonstudents.com>
2025-08-17 13:47:05 +0000

    first page and end index

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from typing import Tuple
+
+
+def index_range(page, page_size):
+
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return start_index, end_index
