4dc5c31ecc11b90003fb1bd6d21d019708f80e5c
D7-57 <10665@holbertonstudents.com>
2025-08-24 18:19:02 +0000

    const or let

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,5 +1,3 @@
-// first task 0-constants.js
-
 export function taskFirst() {
     const task = 'I prefer const when I can.';
     return task;
@@ -12,6 +10,6 @@ export function taskFirst() {
   export function taskNext() {
     let combination = 'But sometimes let';
     combination += getLast();
+  
     return combination;
   }
-  
